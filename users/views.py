2cc7a40f08f9733d2e00b75a102f452a7523297f
GeorginaFox <Georgina.Fox@student.uts.edu.au>
2024-11-07 08:28:33 +1100

    Group Creation

--- users/views.py
+++ users/views.py
@@ -1,9 +1,3 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 from .forms import UserRegistrationForm
 
 def register(request):
@@ -16,25 +10,3 @@ def register(request):
     else:
         form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form': form})
-
-@login_required(login_url='users:login')
-def user(request):
-    return render(request, "users/user.html")
-
-def login_view(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            next_url = request.GET.get('next', reverse("users:user"))
-            return HttpResponseRedirect(next_url)
-        else:
-            messages.error(request, "Invalid Credentials.")
-    return render(request, "users/login.html")
-
-def logout_view(request):
-    logout(request)
-    messages.success(request, "Successfully logged out.")
-    return redirect('users:login')  
