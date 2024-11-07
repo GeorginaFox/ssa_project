dfc0f57fdc278bb5c196f8fcb2601521e16f84af
GeorginaFox <Georgina.Fox@student.uts.edu.au>
2024-11-07 14:59:23 +1100

    fixed user views

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('mfa/', include(tf_urls)),  # Add the two-factor authentication URLs
+    #path('mfa/', include(tf_urls)),  # Add the two-factor authentication URLs
 ]
